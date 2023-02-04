91bc347a0abb0fc36c014e69a7e07bdd02742e33
Sergey <mashinistv@gmail.com>
2023-02-04 08:45:40 +0300

    Task 13 complited

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите целое число: ");
+int.TryParse(Console.ReadLine(), out int number);
+if (number > 99)
+{
+    Console.Write($"Третья цифра в числе: {number}: ");
+    string s = number.ToString();
+    Console.WriteLine(s[2]);
+}
+else Console.WriteLine($"В числе {number} третьей цифры нет");
